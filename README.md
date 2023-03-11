73189eb87eca87b87c02b7b3b21339acaad8678b
vuxnq <vuxnqu@gmail.com>
2023-03-11 14:29:21 +0100

    better

---
+++ README.md
@@ -0,0 +1,13 @@
+## catppuccined
+
+---
+
+batch script for theming images with [catppuccin](https://github.com/catppuccin/catppuccin) palette.
+
+this script depends on [ImageMagick](https://imagemagick.org/script/download.php).
+
+## usage
+
+---
+
+[Download](https://github.com/vuxnq/catppuccined/archive/refs/heads/master.zip) this repository and run catppuccined.bat.
--- catppuccined.bat
+++ catppuccined.bat
@@ -1,7 +1,12 @@
 @echo off
 set /p ImagePath="image location: "
-magick %ImagePath% luts/frappe-hald-clut.png -hald-clut frappe.png
-magick %ImagePath% luts/latte-hald-clut.png -hald-clut latte.png
-magick %ImagePath% luts/macchiato-hald-clut.png -hald-clut macchiato.png
-magick %ImagePath% luts/mocha-hald-clut.png -hald-clut mocha.png
-echo done. & pause > nul
+for /F "delims=" %%i in ("%ImagePath%") do set FileName=%%~ni
+cls
+if not exist export mkdir export
+echo generating images...
+magick "%ImagePath%" luts/frappe-hald-clut.png -hald-clut "export/%FileName%-frappe.png"
+magick "%ImagePath%" luts/latte-hald-clut.png -hald-clut "export/%FileName%-latte.png"
+magick "%ImagePath%" luts/macchiato-hald-clut.png -hald-clut "export/%FileName%-macchiato.png"
+magick "%ImagePath%" luts/mocha-hald-clut.png -hald-clut "export/%FileName%-mocha.png"
+cls
+echo done. & echo results are in "export" directory. & pause > nul
